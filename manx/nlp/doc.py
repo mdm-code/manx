f79e25559e8ad363e3f0021e7f8556a2a0f0317d
mdm-code <code.madamczyk@gmail.com>
2022-05-16 01:21:12 +0200

    [FEAT] Added Token to NLP package
    * I also added more sensible typing hints.

--- manx/nlp/doc.py
+++ manx/nlp/doc.py
@@ -5,23 +5,50 @@ of natural language processing.
 
 # Standard library imports
 from __future__ import annotations
-from typing import Text, TYPE_CHECKING
+from dataclasses import dataclass, field
+from typing import Text, TYPE_CHECKING, TypeVar, Protocol
 import uuid
+
+# Third-party library imports
+import numpy as np
 
 # Local library imports
 if TYPE_CHECKING:
-    from manx.parsing import TagLine
+    from manx.embedding import Model
+    from manx.parsing import POS
 
 
-__all__ = ["ngrams", "Doc", "Span"]
+__all__ = ["Doc", "ngrams", "Token", "Span"]
 
 
-# TODO: Text can represent words as fasttext embedding vectors
-# TODO: Text errors out when prebuilt fasttext model does not exist
+@dataclass(slots=True)
+class Token:
+    lexel: str
+    stripped_lexel: str
+    grammel: str
+    form: str
+    stripped_form: str
+    _pos: POS
+    _model: Model | None = field(repr=False, default=None)
+    _embedding: np.ndarray = field(init=False, repr=False)
+
+    @property
+    def pos(self) -> str:
+        return self._pos.name
+
+    @property
+    def embedding(self) -> np.ndarray | None:
+        if self._model is None:
+            return None
+        if not hasattr(self, "_embedding"):
+            self._embedding = self._model.get_word_vector(self.form)
+        return self._embedding
+
+
 class Doc:
     """Doc object representing a single LAEME document."""
 
-    def __init__(self, elems: list[TagLine], label: str | None = None) -> None:
+    def __init__(self, elems: list[Token], label: str | None = None) -> None:
         self._label = label if label else ""
         self._elems = elems
 
@@ -48,7 +75,7 @@ class Doc:
         return self._uuid.hex
 
     @property
-    def words(self) -> list[TagLine]:
+    def words(self) -> list[Token]:
         return self._elems.copy()
 
     def text(self, *, strip: bool = False) -> Text:
@@ -60,7 +87,7 @@ class Doc:
 class Span:
     """A slice of Doc object."""
 
-    def __init__(self, elems: list[TagLine] | TagLine) -> None:
+    def __init__(self, elems: list[Token] | Token) -> None:
         if isinstance(elems, list):
             self._elems = elems
         else:
@@ -69,12 +96,20 @@ class Span:
     def __len__(self) -> int:
         return len(self._elems)
 
-    def __getitem__(self, i: slice | int) -> TagLine | list[TagLine]:
+    def __getitem__(self, i: slice | int) -> Token | list[Token]:
         return self._elems.copy()[i]
 
 
-def ngrams(source: Doc, *, n: int = 3) -> list[tuple[TagLine, ...]]:
-    result: list[tuple[TagLine, ...]] = list(
+T = TypeVar("T", covariant=True)
+
+
+class Sequenced(Protocol[T]):
+    def __getitem__(self, i: slice | int) -> T | list[T]:
+        raise NotImplementedError
+
+
+def ngrams(source: Sequenced[Token], *, n: int = 3) -> list[tuple[Token, ...]]:
+    result: list[tuple[Token, ...]] = list(
         zip(*[source[n:] for n in range(n)])  # type: ignore
     )
     return result
