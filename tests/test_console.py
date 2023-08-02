cceaaf459af942db06ff5954e4a5fcf442e09fe6
mdm-code <code.madamczyk@gmail.com>
2023-08-03 00:27:53 +0200

    Add remaining two subcommands to CLI unit tests

--- tests/test_console.py
+++ tests/test_console.py
@@ -4,16 +4,36 @@
 import argparse
 from contextlib import nullcontext as does_not_raise
 from dataclasses import dataclass
-from io import BytesIO
+from io import BytesIO, StringIO
 
-# Third-party library imports
+# Third-party imports
 import pytest
 
 # Local library imports
-from manx import console
+from manx import console, nlp, writing
+from manx.model import settings
+from manx.nlp.tokens import Token
+from manx.parsing.tags import POS
+
+
+@pytest.fixture
+def docs() -> list[nlp.Doc]:
+    tokens = [
+        Token(
+            lexel="",
+            stripped_lexel="",
+            grammel="",
+            form="",
+            stripped_form="",
+            sequence=i,
+            _pos=POS.Undef,
+        ) for i in range(100)
+    ]
+    return [nlp.Doc(elems=tokens) for _ in range(10)]
 
 
 def test_console_download(mocker) -> None:
+    """Test if the download subcommand can be invoked from the CLI."""
     opn = mocker.mock_open()
     mkdir = mocker.Mock()
     mocker.patch("builtins.open", opn)
@@ -45,5 +65,41 @@ def test_console_download(mocker) -> None:
         ),
     )
 
+    with does_not_raise():
+        console.main()
+
+
+def test_console_parse(docs: list[nlp.Doc], mocker) -> None:
+    """See if the parse subcommand can be invoked from the CLI."""
+    mocker.patch("manx.console.load", return_value=docs)
+    mocker.patch(
+        "argparse.ArgumentParser.parse_args",
+        return_value=argparse.Namespace(
+            command="parse",
+            from_web=False,
+            verbose=False,
+            root="",
+            output=StringIO(""),
+            format=writing.Format.T5input,
+            ngram_size=settings.DEFAULT_NGRAM_SIZE,
+            chunk_size=settings.DEFAULT_CHUNK_SIZE,
+            prefix=settings.T5_PREFIX,
+        )
+    )
+    with does_not_raise():
+        console.main()
+
+
+def test_console_api(mocker) -> None:
+    """Check if the api subcommand can be invoked from the CLI."""
+    mocker.patch("manx.console.api.run", return_value=None)
+    mocker.patch(
+        "argparse.ArgumentParser.parse_args",
+        return_value=argparse.Namespace(
+            command="api",
+            host=settings.API_HOST,
+            port=settings.API_PORT
+        )
+    )
     with does_not_raise():
         console.main()
